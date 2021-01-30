1bede4e0fefa5918535ad22dd3b9d2ed681b7075
vega2k <vega2k@naver.com>
2021-01-30 14:30:30 +0900

    html 추가

---
+++ src/main/java/jpastudy/jpashop/web/MemberController.java
@@ -0,0 +1,38 @@
+package jpastudy.jpashop.web;
+
+import jpastudy.jpashop.domain.Address;
+import jpastudy.jpashop.domain.Member;
+import jpastudy.jpashop.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class MemberController {
+    @Autowired
+    private MemberService memberService;
+
+    @GetMapping(value = "/members/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    @PostMapping(value = "/members/new")
+    public String create(@Valid MemberForm form, BindingResult result) {
+        if (result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+        Address address = new Address(form.getCity(), form.getStreet(),form.getZipcode());
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+        memberService.join(member);
+        return "redirect:/";
+    }
+}
